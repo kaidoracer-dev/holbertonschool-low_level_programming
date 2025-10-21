ec7f5b26282bfb9b650488cd51619d46296e7149
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-21 10:46:47 +0200

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,11 @@
 
 int main(void)
 {
-	int integerType;
-	printf("Size of int: %zu bytes\n", sizeof(integerType));
-    	return (0);
+	int integerType();
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+    	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of long int: %zu bytes\n", sizeof(long int));
+	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+	return (0);
 }
