18e5d8906a4a78949809aa77754ab2fd80585a5d
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-21 10:31:10 +0200

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int integerType;
+	printf("Size of int: %zu bytes\n", sizeof(integerType));
+    	return (0);
+}
