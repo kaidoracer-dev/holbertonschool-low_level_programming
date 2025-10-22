08e604ac30c64aa3982eb77e1b456bea6c771b8d
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-23 00:17:28 +0200

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char n = 'a';
+
+        for (; n <= 'z'; n++)
+
+                putchar(n);
+
+        char N = 'A';
+
+        for (; N <= 'Z'; N++)
+
+                putchar(N);
+
+        putchar('\n');
+
+        return (0);
+
+
+}
