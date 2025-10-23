5eccc82d83ecb96b49071876529869cb809c8c6c
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-23 13:22:27 +0200

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 'z';
+
+	for (; n >= 'a' ; n--)
+
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+
+
+}
