5455a7b451ad680bb05617ad09f6c4fbb5b9d94a
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-23 12:03:32 +0200

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	for (; n <= '9' ; n++)
+
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+
+
+}
