79a8f1554941772e91cf89e4c5cf28416d43006b
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-23 10:36:13 +0200

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	for (; n <= 'z' ; n++)
+
+	if(n != 'e' && n != 'q')                
+
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+
+
+}
