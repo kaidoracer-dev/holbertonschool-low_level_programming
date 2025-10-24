e3453cd13b4bc9938358da3cf352b0caf724c991
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-24 14:57:47 +0200

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char n = 'a';
 
@@ -14,5 +15,4 @@ int main(void)
 
 	putchar('\n');
 
-	return (0);
 }
