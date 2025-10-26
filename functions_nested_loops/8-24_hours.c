545281d6dd2a9a2807e16c00b33e64d469a22aa9
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-26 22:46:20 +0100

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,11 +3,11 @@
 
 void jack_bauer(void)
 {
-	
-int h, m; 
-	
-	for (h = 0; h < 24; h++) 
-	{	
+
+int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
 		for (m = 0; m < 60; m++)
 		{
 		_putchar('0' + h / 10);
