c42b7471e99d95dbe627ea7ca925730ff4358098
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-28 11:34:43 +0100

    4-print_most_numbers.c y

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_most_numbers - prints the numbers from 0 to 9 except 2 and 4
+ *
+ * Description: This function prints the digits from 0 to 9 using _putchar,
+ * skipping the digits 2 and 4, and then prints a newline at the end.
+ */
 void print_most_numbers(void)
 {
 	char i;
