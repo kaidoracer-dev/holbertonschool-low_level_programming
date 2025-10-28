f753f61b77252b05fcd655738dca79cb50445396
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-28 15:47:25 +0100

    7-print_diagonal.c y

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+int i, j;
+    
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j <= i; j++)
+            _putchar(' ');
+        _putchar('\\');
+        _putchar('\n');
+    }
+}   
