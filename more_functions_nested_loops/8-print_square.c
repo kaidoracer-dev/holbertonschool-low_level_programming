72e369fad50387124c041b0f1248f50ad59e3684
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-29 10:27:06 +0100

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -13,14 +13,16 @@ void print_square(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
+		return;
 	}
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
-				_putchar('\n');
+
 			}
+			_putchar('\n');
 		}
 
 }
