2a344bef9fffdd5fddf29e42165824e1671e27ea
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-29 10:22:27 +0100

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,18 +1,26 @@
 #include "main.h"
-
+/**
+* print_square - Prints a square
+*
+* @size: The size of the square
+*
+*
+*/
 void print_square(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-        for (i = 0; i < size; i++)
-        {
-            for (j = 0; j <= i; j++)
-            _putchar('#');
-            _putchar('\n');
-        }    
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j <= size; j++)
+			{
+				_putchar('#');
+				_putchar('\n');
+			}
+		}
 
 }
