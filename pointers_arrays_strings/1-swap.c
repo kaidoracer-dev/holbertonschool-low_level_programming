b9bd60e27b75227a92cdf9642116540d01d60c60
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-31 14:26:12 +0100

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,15 @@
 #include "main.h"
 /**
-* swap_int - Update the value of two integers
+* swap_int - Swaps the value of two integers
 * @a: pointer to the int to update
+* @b: 
 */
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int save;
+	
+	save = *a;
+	
+	*a = *b;
+	*b = save;
 }
