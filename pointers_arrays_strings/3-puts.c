fb6380663b9441cb577452133021c7718f8ca8fb
kaidoracer-dev <killian.lb@protonmail.com>
2025-10-31 16:11:04 +0100

    main.h

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the string to print
+ *
+ * Return: Nothing
+ */
 void _puts(char *str)
 {
 	int i = 0;
