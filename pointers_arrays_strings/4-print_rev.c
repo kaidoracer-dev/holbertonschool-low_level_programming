a90d902e208ee5dcf7e1df08f96ec0436c85c2e5
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-01 15:57:36 +0100

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,13 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (i[s] != '\0')
+	while (s[i] != '\0')
 
 		i++;
 
 	i--;
 
-	while (i[s] >= 0)
+	while (s[i] >= 0)
 	{
 		_putchar(s[i]);
 		i--;		
