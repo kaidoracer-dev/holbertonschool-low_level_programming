2e51677ec32bc86c8a4f12da28290657d3a694df
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-01 15:42:44 +0100

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i[s] != '\0')
+	{
+		i++;
+	}
+	while (i[s] >= '\0')
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
