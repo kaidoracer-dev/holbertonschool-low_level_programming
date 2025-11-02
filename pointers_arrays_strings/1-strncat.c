54bed2c54df322fa2d74b725e93824771edb5561
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-02 17:50:17 +0100

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatenate src to the end of dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{ 
+		i++;
+	}
+		while (src[i] != '\0' && j < n)
+		{   
+			dest[i] = src[j];
+
+			i++;
+			
+			j++;
+
+
+		}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
