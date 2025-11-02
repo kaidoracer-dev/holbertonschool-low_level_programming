bd92003ffd6e5793227068a27170cb168700795c
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-02 21:55:51 +0100

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,30 +8,27 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
-    int j;
-    char sep[] = " \t\n,;.!?\"(){}";
+	int i = 0;
+	int j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-    /* Capitalize first character if lowercase */
-    if (str[i] >= 'a' && str[i] <= 'z')
-        str[i] = str[i] - ('a' - 'A');
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] = str[i] - ('a' - 'A');
 
-    /* Loop through the string */
-    while (str[i] != '\0')
-    {
-        j = 0;
-        while (sep[j] != '\0')
-        {
-            /* If current char is a separator and next char is lowercase */
-            if (str[i] == sep[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
-            {
-                str[i + 1] = str[i + 1] - ('a' - 'A');
-                break; /* Stop checking other separators */
-            }
-            j++;
-        }
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (sep[j] != '\0')
+		{
+			if (str[i] == sep[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - ('a' - 'A');
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
 
-    return str;
+	return str;
 }
