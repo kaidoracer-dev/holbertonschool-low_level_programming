6153a77b1aa1466a962a8367ca1626a71e6aa351
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-02 16:04:14 +0100

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 1;
+	int t = 0;
+	int f = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			n = n * -1;
+		}
+
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			f = 1;
+			t =(t * 10) + (s[i] - '0');
+		} 
+		else if (f == 1)
+		
+		break;
+		
+		i++;
+	}
+
+	return (t * n);
+}
