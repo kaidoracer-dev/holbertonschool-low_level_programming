26d212dfee46e0abba590f9e35e897153814fdc8
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-03 14:51:32 +0100

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,19 +4,18 @@
 * _strchr - Locates a character in a string
 * @s: The string
 * @c: The character
-* 
-* 
+* Return: adresse of the character
 */
 char *_strchr(char *s, char c)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        i++;
-        
-        if (s[i] == c)  
-           return (&s[i]); 
-    }       
-    return (0);
+	while (s[i] != '\0')
+	{
+		i++;
+		
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
