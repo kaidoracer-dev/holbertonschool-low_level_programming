1d2da428c629433a489b88e1bcb37316afae0b80
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-06 22:19:06 +0100

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to the string to print
+ *
+ * Return: nothing
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
