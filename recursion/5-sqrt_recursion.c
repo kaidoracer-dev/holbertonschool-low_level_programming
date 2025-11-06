92ade64799aca1e20e423c164afeee1e2bbdb744
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-06 22:14:30 +0100

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,26 +2,32 @@
 /**
  * _sqrt_helper - helper function to find the natural square root
  * @n: number to find the square root of
- * @i: current candidate for the square root
+ * @i: current number to test as square root
  *
- * Return: natural square root of n, or -1 if none exists
+ * Return: natural square root, or -1 if none
  */
 int _sqrt_helper(int n, int i)
-{	
+{
 	if (i * i == n)
-	
-		return i;
-	
+
+		return (i);
+
 	if (i * i > n)
-	
-		return -1;
-	
-	return _sqrt_helper(n, i + 1);
+
+		return (-1);
+
+	return (_sqrt_helper(n, i + 1));
 }
 
-int _sqrt_recursion(int n) 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to compute square root of
+ *
+ * Return: natural square root, or -1 if n has no natural sqrt
+ */
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
-	return _sqrt_helper(n, 0);
+		return (-1);
+	return (_sqrt_helper(n, 0));
 }
