d327db24467b5278ff60eb91c5048598cb68a0fb
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-06 21:53:01 +0100

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include  <stdio.h>
 /**
-* _strpbrk - searches a string for any of a set of bytes
-*@i: path the character chain s
-*@j: path the character chain accept
-* return the address of s
-*/
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search in
+ * @accept: the string containing characters to match
+ *
+ * Return: a pointer to the byte in s that matches one of the
+ *         bytes in accept, or NULL if no such byte is found
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i = 0;
