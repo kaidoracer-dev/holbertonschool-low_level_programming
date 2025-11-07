ee29b154c0dfb81c359204593d8c2a8b7f8361e2
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-07 17:49:45 +0100

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
