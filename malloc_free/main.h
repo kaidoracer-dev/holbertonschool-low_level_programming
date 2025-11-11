7c0f0c175e6f6d736ab5eb17f7f724664e000dda
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-11 15:28:11 +0100

    main.h

---
+++ malloc_free/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+    int _putchar(char c);
+    void reset_to_98(int *n);
+    void swap_int(int *a, int *b);
+    int _strlen(char *s);
+    void _puts(char *str);
+    void print_rev(char *s);
+    void rev_string(char *s);
+    void puts2(char *str);
+    void puts_half(char *str);
+    void _puts_recursion(char *s);
+    void _print_rev_recursion(char *s);
+    int _strlen_recursion(char *s);
+    int factorial(int n);
+    int _pow_recursion(int x, int y);
+    int _sqrt_recursion(int n);
+    int is_prime_number(int n);
+    char *create_array(unsigned int size, char c);
+    char *_strdup(char *str);
+    char *str_concat(char *s1, char *s2);
+
+#endif
