ae210fba1160dd5188b63698e05ef910e827051e
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-14 18:48:31 +0100

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,7 +12,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (d == NULL)
 		return;
 
-	d->name = name;
+	d->name = malloc(strlen(name) + 1);
+	strcpy(d->name, name);
+
 	d->age = age;
-	d->owner = owner;
+
+	d->owner = malloc(strlen(owner) + 1);
+	strcpy(d->owner, owner);
+
 }
