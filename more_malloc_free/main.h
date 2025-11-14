ef658a78be39eaa08d149dfb27dd6f9c517f808b
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-14 16:49:06 +0100

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -23,7 +23,7 @@
     int **alloc_grid(int width, int height);
     void free_grid(int **grid, int height);
     void *malloc_checked(unsigned int b);
-    void *_calloc(unsigned int nmemb, unsigned int size);
     char *string_nconcat(char *s1, char *s2, unsigned int n);
+    void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
