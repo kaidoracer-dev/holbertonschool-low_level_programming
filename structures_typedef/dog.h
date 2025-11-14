53014d0c7783b8b713c1ca700272965a040a762b
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-14 18:30:34 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure containing dog info
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+struct  dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
