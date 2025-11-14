6ebc824e0020a3207ca3291b242c14c53355a93f
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-14 18:43:22 +0100

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initializes a dog struct
+ * @d: pointer to the dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
