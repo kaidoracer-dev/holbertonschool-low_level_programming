fb4736abd1c12cc1ab338f4d6fcb01e050ab680f
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-19 18:15:25 +0100

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - calls a function to print a name
  * @name: name to print
@@ -10,6 +11,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-
 	f(name);
 }
