e6e9f460c77abe217f5ba1d4d7305617c7f303a9
kaidoracer-dev <killian.lb@protonmail.com>
2025-11-21 17:32:56 +0100

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -14,6 +14,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	char *value = 0;
 
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
