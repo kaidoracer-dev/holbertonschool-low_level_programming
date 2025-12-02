88faf5ea839775ae477cab54d4de11f9cfed3890
kaidoracer-dev <killian.lb@protonmail.com>
2025-12-02 13:40:42 +0100

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include "lists.h"
+/**
+ * print_list - prints all elements of a list_t linked list
+ * @h: pointer to the first node of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+	h = h->next;
+	i++;
+	}
+	return (i);
+}
+
+
+
+
